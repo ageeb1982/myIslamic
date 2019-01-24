fecfac5c429d43ad43422fc5e33e2c6d0696640b
Mohamed Khidir Ageeb <ageeb1982@gmail.com>
2019-01-24 23:42:34 +0300

    Update1440_05_18

--- lib/screen/TafseerScreen/tafseerScreen.dart
+++ lib/screen/TafseerScreen/tafseerScreen.dart
@@ -111,14 +111,40 @@ class GetTafseerState extends State<GetTafseer> {
   @override
   Widget build(BuildContext context) {
    
-    return Center(child: Text('التفسير موجود'),)
+    //return Center(child: Text('التفسير موجود'),)
+    return      Container(
+          padding: EdgeInsets.all(8.0),
+          color: Color(0xFF131313),
+          height: 50.0,
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                flex: 2,
+                child: TextFormField(
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 11.0),
+                  decoration: InputDecoration(
+                      contentPadding: new EdgeInsets.symmetric(vertical: 0.0),
+                      border: InputBorder.none,
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.all(0.0),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ), // icon is 48px widget.
+                      ),
+                      hintText: 'ابحث عن الآية المراد تفسيرها',
+                      hintStyle: TextStyle(fontSize: 11.0)),
+                ),
+              ),
+            ],
+          ),
+        );    
     
     
     
     
     
     
-    
-    ;
   }
 }
